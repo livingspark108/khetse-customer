dfbf011ec473456f9bb073c716f8f08292acb3e9
Anmol Singh <anmolsingh@syncglob>
2025-05-05 15:13:56 +0530

    show all category products directly

--- lib/screens/sub_categories_screen.dart
+++ lib/screens/sub_categories_screen.dart
@@ -3,9 +3,10 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:get/get.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:user/models/businessLayer/baseRoute.dart';
+import 'package:user/models/categoryProductModel.dart';
+import 'package:user/models/productFilterModel.dart';
 import 'package:user/models/subCategoryModel.dart';
-import 'package:user/screens/productlist_screen.dart';
-import 'package:user/widgets/select_category_card.dart';
+import 'package:user/widgets/products_menu.dart';
 
 class SubCategoriesScreen extends BaseRoute {
   @required
@@ -28,15 +29,14 @@ class SubCategoriesScreen extends BaseRoute {
 
 class _SubCategoriesScreenState extends BaseRouteState {
   int? categoryId;
-  int _selectedIndex = 0;
-  bool _isDataLoaded = false;
-  int? screenId;
   String? screenHeading;
+  bool _isDataLoaded = false;
   bool _isRecordPending = true;
   bool _isMoreDataLoaded = false;
   ScrollController _scrollController = ScrollController();
   int page = 1;
-  List<SubCategory> _subCategoryList = [];
+  List<Product> _productsList = [];
+  ProductFilter _productFilter = new ProductFilter();
 
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
@@ -46,6 +46,7 @@ class _SubCategoriesScreenState extends BaseRouteState {
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text(
           screenHeading!,
@@ -61,51 +62,36 @@ class _SubCategoriesScreenState extends BaseRouteState {
         onRefresh: () async {
           await _onRefresh();
         },
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: _isDataLoaded
-              ? _subCategoryList.length > 0
-                  ? GridView.builder(
+        child: _isDataLoaded
+            ? _productsList.length > 0
+                ? Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: SingleChildScrollView(
                       controller: _scrollController,
-                      itemCount: _subCategoryList.length,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
-                        mainAxisSpacing: 24.0,
-                        crossAxisSpacing: 16.0,
-                        childAspectRatio: 1 / 1,
+                      child: Column(
+                        children: [
+                          ProductsMenu(
+                            analytics: widget.analytics,
+                            observer: widget.observer,
+                            categoryProductList: _productsList,
+                          ),
+                          _isMoreDataLoaded
+                              ? Center(
+                                  child: CircularProgressIndicator(
+                                    backgroundColor: Colors.white,
+                                    strokeWidth: 2,
+                                  ),
+                                )
+                              : SizedBox()
+                        ],
                       ),
-                      itemBuilder: (context, index) => SelectCategoryCard(
-                        key: UniqueKey(),
-                        screenId: 1,
-                        subCategory: _subCategoryList[index],
-                        isSelected: _subCategoryList[index].isSelected,
-                        borderRadius: 0,
-                        onPressed: () {
-                          setState(() {
-                            _subCategoryList
-                                .map((e) => e.isSelected = false)
-                                .toList();
-                            _selectedIndex = index;
-                            if (_selectedIndex == index) {
-                              _subCategoryList[index].isSelected = true;
-                            }
-                          });
-                          Get.to(() => ProductListScreen(
-                                analytics: widget.analytics,
-                                observer: widget.observer,
-                                screenId: 0,
-                                categoryName: _subCategoryList[index].title,
-                                categoryId: _subCategoryList[index].catId,
-                              ));
-                        },
-                      ),
-                    )
-                  : Center(
-                      child: Text(
-                          '${AppLocalizations.of(context)!.txt_nothing_to_show}'),
-                    )
-              : _shimmer(),
-        ),
+                    ),
+                  )
+                : Center(
+                    child: Text(
+                        '${AppLocalizations.of(context)!.txt_nothing_to_show}'),
+                  )
+            : _shimmer(),
       ),
     );
   }
@@ -116,51 +102,9 @@ class _SubCategoriesScreenState extends BaseRouteState {
     _init();
   }
 
-  _getSubCategoryList() async {
-    try {
-      bool isConnected = await br.checkConnectivity();
-      if (isConnected) {
-        if (_isRecordPending) {
-          setState(() {
-            _isMoreDataLoaded = true;
-          });
-          if (_subCategoryList.isEmpty) {
-            page = 1;
-          } else {
-            page++;
-          }
-          await apiHelper.getSubCategory(page, categoryId).then((result) async {
-            if (result != null) {
-              if (result.status == "1") {
-                List<SubCategory> _tList = result.data;
-                if (_tList.isEmpty) {
-                  _isRecordPending = false;
-                }
-                _subCategoryList.addAll(_tList);
-                setState(() {
-                  _isMoreDataLoaded = false;
-                });
-              } else {
-                _isRecordPending = false;
-                setState(() {
-                  _isMoreDataLoaded = false;
-                });
-              }
-            }
-          });
-        }
-      } else {
-        showNetworkErrorSnackBar(_scaffoldKey);
-      }
-    } catch (e) {
-      print("Exception - sub_categories_screen.dart - _getSubCategoryList():" +
-          e.toString());
-    }
-  }
-
   _init() async {
     try {
-      await _getSubCategoryList();
+      await _getAllCategoryProducts();
       _scrollController.addListener(() async {
         if (_scrollController.position.pixels ==
                 _scrollController.position.maxScrollExtent &&
@@ -168,7 +112,7 @@ class _SubCategoriesScreenState extends BaseRouteState {
           setState(() {
             _isMoreDataLoaded = true;
           });
-          await _getSubCategoryList();
+          await _getAllCategoryProducts();
           setState(() {
             _isMoreDataLoaded = false;
           });
@@ -181,10 +125,107 @@ class _SubCategoriesScreenState extends BaseRouteState {
     }
   }
 
+  _getAllCategoryProducts() async {
+    try {
+      bool isConnected = await br.checkConnectivity();
+      if (isConnected) {
+        if (_isRecordPending) {
+          setState(() {
+            _isMoreDataLoaded = true;
+          });
+          if (_productsList.isEmpty) {
+            page = 1;
+          } else {
+            page++;
+          }
+          try {
+            await apiHelper
+                .getSubCategory(page, categoryId)
+                .then((subCatResult) async {
+              if (subCatResult != null && subCatResult.status == "1") {
+                List<SubCategory> subCategories = subCatResult.data;
+
+                if (subCategories.isNotEmpty) {
+                  for (var subCategory in subCategories) {
+                    await apiHelper
+                        .getCategoryProducts(
+                            subCategory.catId, 1, _productFilter)
+                        .then((result) {
+                      if (result != null && result.status == "1") {
+                        List<Product> subCatProducts = result.data;
+                        _productsList.addAll(subCatProducts);
+                      }
+                    });
+                  }
+
+                  if (_productsList.isEmpty) {
+                    _isRecordPending = false;
+                  }
+
+                  setState(() {
+                    _isMoreDataLoaded = false;
+                  });
+                } else {
+                  _getCategoryProductsDirect();
+                }
+              } else {
+                _getCategoryProductsDirect();
+              }
+            });
+          } catch (e) {
+            print("Error fetching subcategories: $e");
+            _getCategoryProductsDirect();
+          }
+        }
+      } else {
+        showNetworkErrorSnackBar(_scaffoldKey);
+      }
+    } catch (e) {
+      print(
+          "Exception - sub_categories_screen.dart - _getAllCategoryProducts():" +
+              e.toString());
+      setState(() {
+        _isMoreDataLoaded = false;
+      });
+    }
+  }
+
+  _getCategoryProductsDirect() async {
+    try {
+      await apiHelper
+          .getCategoryProducts(categoryId, page, _productFilter)
+          .then((result) async {
+        if (result != null) {
+          if (result.status == "1") {
+            List<Product> _tList = result.data;
+            if (_tList.isEmpty) {
+              _isRecordPending = false;
+            }
+            _productsList.addAll(_tList);
+          } else {
+            _isRecordPending = false;
+          }
+        } else {
+          _isRecordPending = false;
+        }
+        setState(() {
+          _isMoreDataLoaded = false;
+        });
+      });
+    } catch (e) {
+      print("Exception - direct product fetch: " + e.toString());
+      setState(() {
+        _isMoreDataLoaded = false;
+        _isRecordPending = false;
+      });
+    }
+  }
+
   _onRefresh() async {
     try {
       _isDataLoaded = false;
       _isRecordPending = true;
+      _productsList.clear();
       setState(() {});
       await _init();
     } catch (e) {
@@ -199,16 +240,16 @@ class _SubCategoriesScreenState extends BaseRouteState {
       child: Shimmer.fromColors(
           baseColor: Colors.grey[300]!,
           highlightColor: Colors.grey[100]!,
-          child: GridView.builder(
-              itemCount: 12,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                mainAxisSpacing: 16.0,
-                crossAxisSpacing: 12.0,
-                childAspectRatio: 0.7,
-              ),
-              itemBuilder: (context, index) =>
-                  SizedBox(height: 130, width: 90, child: Card()))),
+          child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: 10,
+              scrollDirection: Axis.vertical,
+              itemBuilder: (BuildContext context, int index) {
+                return SizedBox(
+                    height: 100 * MediaQuery.of(context).size.height / 830,
+                    width: MediaQuery.of(context).size.width,
+                    child: Card());
+              })),
     );
   }
 }
